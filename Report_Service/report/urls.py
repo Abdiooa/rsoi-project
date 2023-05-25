b36cda4c706e9744ec2ab95698f07307790ee313
Abdi <112772257+Abdiaoo@users.noreply.github.com>
2023-05-25 15:36:07 +0300

    Update urls.py

--- Report_Service/report/urls.py
+++ Report_Service/report/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import report_by_users,report_by_hotels,report_by_booking
 
 urlpatterns = [
+    path('booking', report_by_booking), 
     path('users', report_by_users),
     path('hotels', report_by_hotels),
-    path('booking', report_by_booking), 
 ]
